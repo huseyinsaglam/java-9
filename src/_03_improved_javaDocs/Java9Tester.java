e7eb3af8aa99f9c552e68682441edfc3ed173619
hüseyin <hsaglam001@gmail.com>
2023-02-02 13:44:45 +0300

    Java 9 - Improved JavaDocs (#2)
    * JShell
    
    * Java 9 - Improved JavaDocs
    
    ---------

---
+++ src/_03_improved_javaDocs/Java9Tester.java
@@ -0,0 +1,13 @@
+package _03_improved_javaDocs;
+
+public class Java9Tester {
+
+    /**
+     * Default method to be run to print
+     * <p>Hello world</p>
+     * @param args command line arguments
+     */
+    public static void main(String []args) {
+        System.out.println("Hello World");
+    }
+}
